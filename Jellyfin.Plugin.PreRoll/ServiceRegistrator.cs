f3928d39f1cf39038c0d9efb9daeb16df7d76e53
justhallwest-commits <justhallwest@gmail.com>
2026-03-24 22:02:21 -0400

    Update ServiceRegistrator.cs

--- Jellyfin.Plugin.PreRoll/ServiceRegistrator.cs
+++ Jellyfin.Plugin.PreRoll/ServiceRegistrator.cs
@@ -1,18 +1,15 @@
 using MediaBrowser.Controller;
-using MediaBrowser.Controller.Library;
 using MediaBrowser.Controller.Plugins;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Jellyfin.Plugin.PreRoll;
 
-/// <summary>
-/// Registers plugin services with Jellyfin's dependency-injection container.
-/// </summary>
 public class ServiceRegistrator : IPluginServiceRegistrator
 {
-    /// <inheritdoc />
-    public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
+    public void RegisterServices(
+        IServiceCollection serviceCollection,
+        IServerApplicationHost applicationHost)
     {
-        serviceCollection.AddSingleton<IIntroProvider, PreRollIntroProvider>();
+        serviceCollection.AddHostedService<PreRollServerEntryPoint>();
     }
 }
